0f9a46c6889acb15c3c505b62d9800236ee83cec
shaun <181657675@qq.com>
2021-03-09 15:24:19 +0800

    ver0.2

--- DAY2-4.py
+++ DAY2-4.py
@@ -5,17 +5,16 @@ depart2_m = 'qinke'
 COURSE_FEES_SEC = 456789.12456
 COURSE_FEES_Python = 1234.3456
 
-line1 = "Department1 name:%-11sManager:%-11sCOURSE FEES:%-11.2fThe End!" \
-        % (department1, depart1_m, COURSE_FEES_SEC)
-line2 = "Department1 name:%-11sManager:%-11sCOURSE FEES:%-11.2fThe End!" \
-        % (department2, depart2_m, COURSE_FEES_Python)
+# line1 = "Department1 name:%-11sManager:%-11sCOURSE FEES:%-11.2fThe End!" \
+#         % (department1, depart1_m, COURSE_FEES_SEC)
+# line2 = "Department1 name:%-11sManager:%-11sCOURSE FEES:%-11.2fThe End!" \
+#         % (department2, depart2_m, COURSE_FEES_Python)
+
+line1 = 'Department1 name:{0:<11}Manager:{1:<11}COURSE FEES:{2:<11.2f}The End!'.format(department1, depart1_m, COURSE_FEES_SEC)
+line2 = 'Department1 name:{0:<11}Manager:{1:<11}COURSE FEES:{2:<11.2f}The End!'.format(department2, depart2_m, COURSE_FEES_Python)
 
 length = len(line1)
 print('=' * length)
 print(line1)
 print(line2)
 print('=' * length)
-
-
-# line1 = '{0:<30} {1:<20} {2:<25} {3:8}'.format('"Department1 name:', 'Manager:', 'COURSE FEES:', 'The End!')
-# print(line1)
