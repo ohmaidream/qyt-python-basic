7268f6b9ea635993feca0f6cdfc23ce4431fa35d
shaun <181657675@qq.com>
2021-03-16 16:29:17 +0800

    ver0.2

--- DAY8-1.py
+++ DAY8-1.py
@@ -2,12 +2,16 @@ import re
 import time
 import os
 
+trigger = 'null'
 while True:
-    try:
-        time.sleep(2)
-        netstat_result = os.popen("netstat -tulnp").read()
-        print("等待一秒重新开始监控！")
-    except re.match('.*tcp.*(0.0.0.0:80)\s+.*', netstat_result):
-        print("HTTP(TCP/80)服务已经被打开")
+    time.sleep(2)
+    print("等待一秒重新开始监控！")
+    for netstat_result in os.popen("netstat -tulnp").read().split("\n"):
+        if re.match('.*tcp.*0.0.0.0:80\s+.*', netstat_result):
+            print("HTTP(TCP/80)服务已经被打开")
+            trigger = 'break'
+    if trigger == 'break':
         break
+
+
 
