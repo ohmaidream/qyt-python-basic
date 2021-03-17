da0aca81f4a82846428e14eb86a12f873f041d2d
shaun <181657675@qq.com>
2021-03-17 19:59:30 +0800

    ver0.1

---
+++ DAY8-3.py
@@ -0,0 +1,18 @@
+import logging
+logging.getLogger("kamene.runtime").setLevel(logging.ERROR)
+from kamene.all import *
+
+def qytang_ping(ip):
+    ping_pkt = IP(dst=ip) / ICMP()
+    ping_result = sr1(ping_pkt, timeout=2, verbose=False)
+    if ping_result:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    result = qytang_ping('192.168.1.254')
+    if result == True:
+        print("request is reachable")
+    elif result == False:
+        print("no response")
---
+++ level1/__init__.py
@@ -0,0 +1,4 @@
+a = 100
+def printer(x):
+    print(f'level1 - {x}')
+
